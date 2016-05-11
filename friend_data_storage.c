59202a136f22179950467da29d45c384b1396a2e
Scott Davison <scott@jewelbots.com>
2016-05-11 13:25:46 -0400

    no while loops = still working

--- friend_data_storage.c
+++ friend_data_storage.c
@@ -8,9 +8,9 @@
 #define DATA_STORAGE_TYPE_ID            0x0021
 #define SENTINEL_VALUE                  0xEE
 
-static uint8_t write_flag=0;
-static uint8_t clear_flag=0;
-static uint8_t gc_flag=0;
+//static uint8_t write_flag=0;
+//static uint8_t clear_flag=0;
+//static uint8_t gc_flag=0;
 
 static void fds_evt_handler(ret_code_t          result,
                             fds_cmd_id_t        cmd,
@@ -35,14 +35,14 @@ if (result != 0) {
             break;
         case FDS_CMD_WRITE:
             SEGGER_RTT_printf(0, "FDS_CMD_WRITE: result: %u, instance: %02x, type: %02x\n", result, record_key.instance, record_key.type);
-            write_flag=1;
+            //write_flag=1;
 						//err_code = fds_gc();
             //APP_ERROR_CHECK(err_code);
             break;
 
         case FDS_CMD_CLEAR:
             SEGGER_RTT_WriteString(0, "FDS_CMD_CLEAR\n");
-						clear_flag=1;
+						//clear_flag=1;
             break;
 
         case FDS_CMD_CLEAR_INST:
@@ -53,7 +53,7 @@ if (result != 0) {
 
         case FDS_CMD_GC:
             SEGGER_RTT_WriteString(0, "GC FDS\n");
-						gc_flag=1;
+						//gc_flag=1;
             break;
 
         default:
@@ -75,7 +75,7 @@ void initialize_friends_list_in_flash(friends_list_t * friends_list) {
 	fds_record_desc_t friend_list_descriptor;
   fds_find_token_t tok;
 
-	write_flag=0;
+	//write_flag=0;
   bool has_records = false;
 
   while(fds_find(DATA_STORAGE_TYPE_ID, DATA_STORAGE_INSTANCE_ID, &friend_list_descriptor, &tok) == NRF_SUCCESS) {
@@ -94,7 +94,7 @@ void initialize_friends_list_in_flash(friends_list_t * friends_list) {
     err_code = fds_write(&friend_list_descriptor, key, 1, &chunk);
     SEGGER_RTT_printf(0, "INITIAL WRITE FRIENDS: max_length: %u, length_words: %u, Error Code was: %u\n", max_length, length_words, err_code);
     APP_ERROR_CHECK(err_code);
-		while(write_flag==0);
+		//while(write_flag==0);
   }
 }
 
@@ -107,9 +107,9 @@ void save_friends(friends_list_t * friends_list) {
 	key.type = DATA_STORAGE_TYPE_ID;
 	key.instance = DATA_STORAGE_INSTANCE_ID;
 	
-	write_flag=0;
-	clear_flag=0;
-	gc_flag=0;
+	//write_flag=0;
+	//clear_flag=0;
+	//gc_flag=0;
 
 
 	chunk.p_data = friends_list;
@@ -125,16 +125,16 @@ void save_friends(friends_list_t * friends_list) {
 			err_code = fds_clear(&friend_list_descriptor);
 			SEGGER_RTT_WriteString(0, "Clear existing record.\n");
 			APP_ERROR_CHECK(err_code);
-			while(clear_flag==0);
+			//while(clear_flag==0);  //Not sure if this is necessary, not in the nrf52/SDK11 example
 	}
 	err_code = fds_gc();
   APP_ERROR_CHECK(err_code);
-	while(gc_flag==0);
+	//while(gc_flag==0);  //Not sure if this is necessary, not in the nrf52/SDK11 example
 	SEGGER_RTT_WriteString(0, "Run Garbage Collect.\n");
 	err_code = fds_write(&friend_list_descriptor, key, 1, &chunk);
 	SEGGER_RTT_printf(0, "WRITING UPDATED FRIENDS: max_length: %u, length_words: %u, Error Code was: %u\n", max_length, length_words, err_code);
 	APP_ERROR_CHECK(err_code);
-	while(write_flag==0);
+	//while(write_flag==0);
 }
 
 void load_friends(friends_list_t * friends_to_load)
